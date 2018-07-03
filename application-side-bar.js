78cf47cd4444280a6a58e64128af940d3223253f
Virendra Padate <virendra.padate@tcs.com>
2018-07-03 12:53:34 +0530

    Data binding

--- application-side-bar.js
+++ application-side-bar.js
@@ -2,12 +2,6 @@ import {
   PolymerElement,
   html
 } from '@polymer/polymer/polymer-element.js';
-import '@polymer/app-layout/app-drawer/app-drawer.js';
-import '@polymer/app-layout/app-drawer-layout/app-drawer-layout.js';
-import '@polymer/app-layout/app-toolbar/app-toolbar.js';
-import '@polymer/iron-pages/iron-pages.js';
-import '@polymer/iron-selector/iron-selector.js';
-
 
 /**
  * `application-side-bar`
@@ -42,12 +36,12 @@ class ApplicationSideBar extends PolymerElement {
           font-weight: bold;
         }
       </style>
-        <app-toolbar>Menu</app-toolbar>
-        <iron-selector selected="[[page]]" attr-for-selected="name" class="drawer-list" role="navigation">
-          <a name="view1" href="[[rootPath]]view1">View One</a>
-          <a name="view2" href="[[rootPath]]view2">View Two</a>
-          <a name="view3" href="[[rootPath]]view3">View Three</a>
-        </iron-selector>
+      <app-toolbar>Menu</app-toolbar>
+      <iron-selector selected="[[page]]" attr-for-selected="name" class="drawer-list" role="navigation">
+        <a name="view1" href="[[rootPath]]view1" on-click="handleClick">View One</a>
+        <a name="view2" href="[[rootPath]]view2" on-click="handleClick">View Two</a>
+        <a name="view3" href="[[rootPath]]view3" on-click="handleClick">View Three</a>
+      </iron-selector>
     `;
   }
   static get properties() {
@@ -56,7 +50,16 @@ class ApplicationSideBar extends PolymerElement {
         type: String,
         value: 'application-side-bar',
       },
+      selectedMenu: {
+        type: String,
+        notify: true,
+        value: 'Hello'
+      }
     };
+  }
+  handleClick(e) {
+    console.log(e.target.name);
+    this.selectedMenu = e.target.name;
   }
 }
 
